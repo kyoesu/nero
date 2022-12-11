5c60e2403173a496fee2a10c160912d864a2a189
Esu <muklinkulov.r@gmail.com>
2022-12-12 02:26:31 +0400

    add xlsx(craete db)

--- cycle.py
+++ cycle.py
@@ -6,13 +6,13 @@ j=0
 z=0
 f=open('ww.txt','r+') 
 a=""
-print(time.perf_counter())
+t=time.perf_counter()
 for i in range(10000):
     for j in range(10000):
         z+=1
         
     
-a=str(z)+' итераций пройдено, '+str(time.perf_counter())+' секунд прошло \n'
+a=str(z)+' итераций пройдено, '+str(time.perf_counter()-t)+' секунд прошло \n'
 f.write(a)
 print (a)
 
